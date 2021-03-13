332538b3ab71bc5df9f46aa15ff14a19aae18577
Elwin <elwin.carlos09@gmail.com>
2021-03-13 10:46:36 +0000

    add test suites

---
+++ client/test/layout.test.js
@@ -0,0 +1,21 @@
+//Import two built-in methods to bring in HTML file for testing
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve((__dirname, './index.html'), 'utf8'));
+
+//Set up the tests
+describe('index.html', () => {
+    beforeEach(() => {
+        document.documentElement.innerHTML = html.toString();
+    })
+
+    test('it has a header with the word welcome in it', () => {
+        let header = document.querySelector('header');
+        expect(header.textContent).toContain('welcome');
+    })
+
+    test('it has a footer with an image reference', () => {
+        let footer = document.querySelector('footer');
+        expect(footer.textContent).toContain('ref');
+    })
+})
